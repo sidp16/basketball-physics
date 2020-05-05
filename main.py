151f22b9f6520872c21afbc9a4bc85bf7b815d8e
sidp16 <63413238+sidp16@users.noreply.github.com>
2020-05-06 00:22:47 +0100

    Created different shapes in different colours in a pygame window

--- main.py
+++ main.py
@@ -13,10 +13,16 @@ green = (0,255,0)
 blue = (0,0,255)
 
 gameDisplay = pygame.display.set_mode((800,600))#
-gameDisplay.fill(black)
+gameDisplay.fill(white)
 
 pixAr = pygame.PixelArray(gameDisplay)
+
 pixAr[10][20] = green
+
+pygame.draw.line(gameDisplay, blue, (100,200), (300,450),5)
+pygame.draw.rect(gameDisplay,red, (400,400,50,25))
+pygame.draw.circle(gameDisplay, green, (150,150),75)
+pygame.draw.polygon(gameDisplay,blue, ((25,75),(76,125),(700,120)))
 
 while True:
     for event in pygame.event.get():
