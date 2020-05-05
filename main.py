7871cec257113fe04502898c551da5e61a5c2fd2
sidp16 <63413238+sidp16@users.noreply.github.com>
2020-05-06 00:14:16 +0100

    Showed a green pixel on a pygame window

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("commit 3")
+import pygame
+
+pygame.init()
+
+display_width = 800
+display_height = 600
+
+white = (255,255,255)
+black = (0,0,0)
+
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+
+gameDisplay = pygame.display.set_mode((800,600))#
+gameDisplay.fill(black)
+
+pixAr = pygame.PixelArray(gameDisplay)
+pixAr[10][20] = green
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    pygame.display.update()
