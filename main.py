217a3bf920b439a1fa308ae03709fc63352f79b0
sidp16 <63413238+sidp16@users.noreply.github.com>
2020-05-06 12:04:18 +0100

    Created a single circle, centered and orange. Ready to code the 'falling' action.

--- main.py
+++ main.py
@@ -11,18 +11,15 @@ black = (0,0,0)
 red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
+orange = (255,165,0)
 
 gameDisplay = pygame.display.set_mode((800,600))#
 gameDisplay.fill(white)
+pygame.display.set_caption("Ball Physics Simulation")
 
 pixAr = pygame.PixelArray(gameDisplay)
 
-pixAr[10][20] = green
-
-pygame.draw.line(gameDisplay, blue, (100,200), (300,450),5)
-pygame.draw.rect(gameDisplay,red, (400,400,50,25))
-pygame.draw.circle(gameDisplay, green, (150,150),75)
-pygame.draw.polygon(gameDisplay,blue, ((25,75),(76,125),(700,120)))
+pygame.draw.circle(gameDisplay, orange, (400,300),50)
 
 while True:
     for event in pygame.event.get():
