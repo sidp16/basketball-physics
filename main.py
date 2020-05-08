a740fc3117464454d98de32808927a033101d95a
sidp16 <63413238+sidp16@users.noreply.github.com>
2020-05-08 15:39:51 +0100

    Ball falls and bounces realistically using p

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ clock = pygame.time.Clock()
 display_width = 800
 display_height = 600
 acceleration = 0.5
-nAcceleration = 1
-speed = 0
+velocity = 0
+bounce = 0.5**0.5
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 gameDisplay.fill((64, 64, 64))
@@ -24,22 +24,23 @@ while True:
             quit()
 
     while basketball.y < display_height - basketball.radius:
-        basketball.y = min(basketball.y + speed, display_height - basketball.radius)
-        speed += acceleration
+        basketball.y = min(basketball.y + velocity, display_height - basketball.radius)
+        velocity += acceleration
         gameDisplay.fill((64, 64, 64))
         basketball.draw(gameDisplay)
         clock.tick(60)
         pygame.display.update()
-        print("STOPPED: %s" % speed)
-    speed = -speed
-    while speed != 0:
-        print("BOUNCE: %s" % speed)
-        basketball.y += speed
-        speed += nAcceleration
+        print(f"speed: {velocity} | pos: {basketball.y} | going down")
+
+    velocity = -velocity * bounce
+    if abs(velocity) < 2.15:
+        continue
+
+    while velocity < 0:
+        print(f"speed: {velocity} | pos: {basketball.y} | going up")
+        basketball.y += velocity
+        velocity += acceleration
         gameDisplay.fill((64, 64, 64))
         basketball.draw(gameDisplay)
         clock.tick(60)
         pygame.display.update()
-
-
-
