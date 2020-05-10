e584541fdf63228e773f542339db7c17b57990e2
sidp16 <63413238+sidp16@users.noreply.github.com>
2020-05-10 18:50:02 +0100

    Made the falling / bouncing a feature of any ball created

--- ball.py
+++ ball.py
@@ -16,4 +16,35 @@ class Ball:
     def draw(self, window):
         x, y = int(self.x), int(self.y)
         pygame.draw.circle(window, BLACK, (x, y), self.radius)
-        pygame.draw.circle(window, self.colour, (x, y), self.radius - 3)
+        pygame.draw.circle(window, self.colour, (x, y), self.radius - 4)
+
+    def update(self, velocity, acceleration, bounce, display_height, gameDisplay):
+        self.velocity = velocity
+        self.acceleration = acceleration
+        self.bounce = bounce
+        self.display_height = display_height
+        clock = pygame.time.Clock()
+
+        while True:
+            while self.y < display_height - self.radius:
+                self.y = min(self.y + velocity, display_height - self.radius)
+                velocity += acceleration
+                gameDisplay.fill((64, 64, 64))
+                self.draw(gameDisplay)
+                clock.tick(60)
+                pygame.display.update()
+                print(f"speed: {velocity} | pos: {self.y} | going down")
+
+            velocity = -velocity * bounce
+            if abs(velocity) < 2.15:
+                continue
+
+            while velocity < 0:
+                print(f"speed: {velocity} | pos: {self.y} | going up")
+                self.y += velocity
+                velocity += acceleration
+                gameDisplay.fill((64, 64, 64))
+                self.draw(gameDisplay)
+                clock.tick(60)
+                pygame.display.update()
+
--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import pygame
 from ball import Ball
-from colours import ORANGE
+from colours import ORANGE, GREEN, RED
 
 pygame.init()
 clock = pygame.time.Clock()
 display_width = 800
 display_height = 600
 acceleration = 0.5
-velocity = 0
+velocity = 0.1
 bounce = 0.5**0.5
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 gameDisplay.fill((64, 64, 64))
 pygame.display.set_caption("Ball Physics Simulation")
 
-basketball = Ball(400, 10, 35, ORANGE)
+basketball = Ball(400, 40, 35, ORANGE)
 basketball.draw(gameDisplay)
 
 while True:
@@ -23,24 +23,5 @@ while True:
             pygame.quit()
             quit()
 
-    while basketball.y < display_height - basketball.radius:
-        basketball.y = min(basketball.y + velocity, display_height - basketball.radius)
-        velocity += acceleration
-        gameDisplay.fill((64, 64, 64))
-        basketball.draw(gameDisplay)
-        clock.tick(60)
-        pygame.display.update()
-        print(f"speed: {velocity} | pos: {basketball.y} | going down")
+        basketball.update(velocity, acceleration, bounce, display_height, gameDisplay)
 
-    velocity = -velocity * bounce
-    if abs(velocity) < 2.15:
-        continue
-
-    while velocity < 0:
-        print(f"speed: {velocity} | pos: {basketball.y} | going up")
-        basketball.y += velocity
-        velocity += acceleration
-        gameDisplay.fill((64, 64, 64))
-        basketball.draw(gameDisplay)
-        clock.tick(60)
-        pygame.display.update()
