657551f426868a2e00c42f0b0b7dcb20bcd5c320
Siddharth Prasad <siddharthp@Siddharths-MacBook-Pro.local>
2020-05-20 12:58:45 +0100

    Can now apply a force + angle to the ball and it calculates the X and Y forces respectively.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from math import degrees, sin, cos
-
 import pygame
+
+from actions import shootBall
 from ball import Ball
 from colours import ORANGE
 from config import DISPLAY_WIDTH, DISPLAY_HEIGHT
@@ -8,17 +8,9 @@ from config import DISPLAY_WIDTH, DISPLAY_HEIGHT
 def clearScreen():
     gameDisplay.fill((255, 255, 255))
 
-# force in newtons, angle in degrees
-def shootBall(ball, totalForce, angle):
-    print(f"Angle:  {angle}")
-    y_force = sin(angle) * totalForce
-    x_force = cos(angle) * totalForce
-    print(f"X force: {x_force}, Y force: {y_force}")
-    ball.addForce(x_force, y_force)
-
-
 pygame.init()
 bounce = 0.5 ** 0.5
+clock = pygame.time.Clock()
 
 gameDisplay = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 pygame.display.set_caption("Ball Physics Simulation")
@@ -26,9 +18,8 @@ clearScreen()
 
 basketball = Ball(x=832, y=680, radius=20, colour=ORANGE, bounciness=bounce)
 basketball.draw(gameDisplay)
-clock = pygame.time.Clock()
 
-shootBall(basketball,500,70)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,8 +35,7 @@ while True:
             if event.key == pygame.K_RIGHT:
                 basketball.addForce(x=350)
             if event.key == pygame.K_SPACE:
-                basketball.addForce(y=-2000)
-
+                shootBall(basketball, 3000, int(45))
 
     dt = clock.tick(60) # in ms, e.g 2500ms is 2.5s (diff in time since last iteration)
     factor = 0.5/100
