3ebe24b4afbede39cd5e3c8a5a04f207f246ca56
Siddharth Prasad <siddharthp@Siddharths-MacBook-Pro.local>
2020-05-25 17:34:30 +0100

    Can find distance between impact and end point of wall

--- ball.py
+++ ball.py
@@ -1,5 +1,5 @@
 import pygame
-from math import sqrt, degrees, acos, sin
+from math import sqrt, degrees, acos, sin, cos
 
 from colours import BLACK, RED, ORANGE, GREEN
 from config import DISPLAY_HEIGHT, DISPLAY_WIDTH
@@ -34,11 +34,12 @@ class Ball:
         return self.x >= DISPLAY_WIDTH - self.radius or self.x <= 0 + self.radius
 
     def isTouchingWall(self, wall):
+        global distP, distB
         length = sqrt((wall.startPos[0] - wall.endPos[0])**2 + (wall.startPos[1] - wall.endPos[1])**2)
         distT = sqrt((self.x - wall.startPos[0])**2 + (self.y - wall.startPos[1])**2)
         distB = sqrt((self.x - wall.endPos[0])**2 + (self.y - wall.endPos[1])**2)
 
-        # ( b^2 + c^2 - a^2 ) / (2 * b * c) > cosine rule
+        # ( b^2 + c^2 - a^2 ) / (2 * b * c) > cosine rule (angle)
         top = ((distT**2) + (length**2) - (distB**2)) / (2*distT*length)
         bottom = ((distB**2) + (length**2) - (distT**2)) / (2*distB*length)
 
@@ -49,13 +50,12 @@ class Ball:
 
         if degrees(angT) <= 90 and degrees(angB) <= 90:
             distP = distT * sin(angT)
-            print(f"distP: {distP:4.2f}, t: {degrees(angT):4.2f}, b: {degrees(angB): 4.2f}")
+            # print(f"distP: {distP:4.2f}, t: {degrees(angT):4.2f}, b: {degrees(angB): 4.2f}")
             self.colour = ORANGE
             return distP <= self.radius
         else:
             self.colour = RED
             return False
-        # print(f"dT: {distT:4.2f}, dB: {distB:4.2f}")
 
     def addForce(self, x=0, y=0):
         if x:
@@ -67,6 +67,12 @@ class Ball:
 
             self.aY = resultant_force / self.mass
 
+    def resetPosition(self):
+        # c^2 = a^2 + b^2 - 2ab * cos(C) > cosine rule (side)
+        angP = acos(distP / distB)
+        distX = (distB ** 2) + (distP ** 2) - (2 * distB * distP) * cos(angP)
+
+        return distX
     def update(self, dt):
         self.vY += self.aY * dt
         self.y = min(self.y + self.vY * dt, DISPLAY_HEIGHT - self.radius)
@@ -91,7 +97,9 @@ class Ball:
 
         for w in walls:
             if self.isTouchingWall(w):
+                distX = self.resetPosition()
+                print(f"distX: {distX:4.2f}")
                 self.colour = GREEN
+                self.vX = -self.vX * self.bounce
+                self.vY = -self.vY * self.bounce
                 break
-                # self.vX = -self.vX * self.bounce
-                # self.vY = -self.vY * self.bounce
--- gameObjects.py
+++ gameObjects.py
@@ -3,6 +3,6 @@ from colours import PURPLE
 from wall import Wall
 
 wall = Wall(startPos=(800,100), endPos=(100,500), colour=PURPLE, width=1)
-wall2 = Wall(startPos=(1200,400), endPos=(900,100), colour=PURPLE, width=1)
-wall3 = Wall(startPos=(600,600), endPos=(500,400), colour=PURPLE, width=1)
-walls = [wall,wall2,wall3]
+# wall2 = Wall(startPos=(1200,400), endPos=(900,100), colour=PURPLE, width=1)
+# wall3 = Wall(startPos=(600,600), endPos=(500,400), colour=PURPLE, width=1)
+walls = [wall]
